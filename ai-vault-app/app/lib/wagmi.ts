3a5a1e061df6675f063d2c4b2570ebde5f4026d1
--get <vectorsats02ozo@gmail.com>
2026-03-19 02:01:18 +0100

    remove the entire frontend dir and re-did it coz tailwinf for some reason wasn't loading properly

---
+++ ai-vault-app/app/lib/wagmi.ts
@@ -0,0 +1,9 @@
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { sepolia } from "wagmi/chains";
+
+export const config = getDefaultConfig({
+  appName: "AIVault",
+  projectId: "a2c929670a47c3e50791389a3b9efc9c",
+  chains: [sepolia],
+  ssr: true,
+});
